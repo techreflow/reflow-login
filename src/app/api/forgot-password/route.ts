b0cfd20cfb518d01e90411f8ed2aef535cea1f85
prashant-2204 <pk6710@srmist.edu.in>
2024-08-08 17:12:48 +0530

    changes

--- src/app/api/forgot-password/route.ts
+++ src/app/api/forgot-password/route.ts
@@ -41,8 +41,8 @@ export async function POST(req: NextRequest) {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.NEXT_PUBLIC_EMAIL_USER,
+        pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
       },
     });
 
